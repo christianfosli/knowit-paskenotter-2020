f7bf81ab9a73ebab315b35aa8592e179347a3726
Christian Fosli <cfosli@gmail.com>
2020-04-09 19:19:23 +0200

    Ignore the huge textfile

--- luke1/src/main.rs
+++ luke1/src/main.rs
@@ -1,5 +1,10 @@
 use std::fs;
 
+// Problem:
+// This file contains numbers from 1 to 100_000, except for one number.
+// which number is missing?
+// https://gist.githubusercontent.com/knowitkodekalender/f962392f90e181377dd51de6abe3b480/raw/c0ef3c2762d3f7bf9016b06d2d6b1fdce58f36cd/numbers.txt
+
 fn main() {
     let numbers = fs::read_to_string("numbers.txt").unwrap();
     let missing = find_missing_number(100_000, &numbers);
