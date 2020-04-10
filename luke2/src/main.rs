a283802174f09ef455fd1865ecb3ed5194cb5c6a
Christian Fosli <cfosli@gmail.com>
2020-04-10 23:07:54 +0200

    Fix spelling

--- luke2/src/main.rs
+++ luke2/src/main.rs
@@ -1,12 +1,12 @@
 use std::{cmp::Reverse, collections::BinaryHeap};
 
 fn main() {
-    let mut laylandtall = BinaryHeap::new();
+    let mut leylandtall = BinaryHeap::new();
     let mut x = 2;
     let mut y = 2;
     loop {
-        let lt = calc_laylandtall(x, y);
-        laylandtall.push(Reverse(lt));
+        let lt = calc_leylandtall(x, y);
+        leylandtall.push(Reverse(lt));
 
         if (x).checked_pow((y + 1) as u32).is_some() {
             y += 1;
@@ -18,7 +18,7 @@ fn main() {
                 "only overflows left.... exiting with x {}, y {}, count {}",
                 x,
                 y,
-                laylandtall.len()
+                leylandtall.len()
             );
             break;
         }
@@ -26,13 +26,13 @@ fn main() {
 
     let mut sum = 0;
     for _ in 0..250 {
-        let smallest = laylandtall.pop().unwrap().0;
+        let smallest = leylandtall.pop().unwrap().0;
         sum += smallest;
     }
     println!("sum: {}", sum);
 }
 
-fn calc_laylandtall(x: u128, y: u128) -> u128 {
+fn calc_leylandtall(x: u128, y: u128) -> u128 {
     x.pow(y as u32) + y.pow(x as u32)
 }
 
@@ -41,7 +41,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn laylandtall_given17_shouldbetrue() {
-        assert_eq!(17, calc_laylandtall(3, 2))
+    fn leylandtall_given17_shouldbetrue() {
+        assert_eq!(17, calc_leylandtall(3, 2))
     }
 }
