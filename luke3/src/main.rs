e66a496ebf5642e0f32f56e8934df0e55b62f981
Christian Fosli <cfosli@gmail.com>
2020-04-11 14:35:42 +0200

    Functional version

--- luke3/src/main.rs
+++ luke3/src/main.rs
@@ -2,24 +2,18 @@ use radix_fmt::radix;
 
 fn main() {
     let count = (1u32..1_000_000)
-        .filter(|n: &u32| is_multibase_palindrome(*n))
+        .filter(|n| is_multibase_palindrome(*n))
         .count();
 
     println!("{}", count);
 }
 
 fn is_multibase_palindrome(n: u32) -> bool {
-    let mut count = 0;
-    for base in 2..11 {
-        let string_repr = format!("{}", radix(n, base));
-        if is_palindrome(&string_repr) {
-            count += 1;
-            if count == 2 {
-                return true;
-            }
-        }
-    }
-    false
+    (2u8..11)
+        .map(|base| format!("{}", radix(n, base)))
+        .filter(|formatted| is_palindrome(formatted))
+        .count()
+        >= 2
 }
 
 fn is_palindrome(s: &str) -> bool {
