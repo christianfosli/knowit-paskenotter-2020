3f02438974e799a270c6eb4b5f6fd696ccce679e
Christian Fosli <cfosli@gmail.com>
2020-04-11 14:29:53 +0200

    We got it!

--- luke3/src/main.rs
+++ luke3/src/main.rs
@@ -1,3 +1,37 @@
+use radix_fmt::radix;
+
 fn main() {
-    println!("Hello, world!");
+    let count = (1u32..1_000_000)
+        .filter(|n: &u32| is_multibase_palindrome(*n))
+        .count();
+
+    println!("{}", count);
+}
+
+fn is_multibase_palindrome(n: u32) -> bool {
+    let mut count = 0;
+    for base in 2..11 {
+        let string_repr = format!("{}", radix(n, base));
+        if is_palindrome(&string_repr) {
+            count += 1;
+            if count == 2 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn is_palindrome(s: &str) -> bool {
+    s == s.chars().rev().collect::<String>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_palindrome_should_be_true() {
+        assert!(is_palindrome("131"));
+    }
 }
