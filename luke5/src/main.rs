6780fcfbace3c53908248528767ce9856caec22a
Christian Fosli <cfosli@gmail.com>
2020-04-13 20:13:28 +0200

    Add parallelization with Rayon

--- luke5/src/main.rs
+++ luke5/src/main.rs
@@ -1,6 +1,10 @@
+use rayon::prelude::*;
+
 fn main() {
     let sum: u128 = (1u128..10_000_001)
-        .filter(|n| n.becomes_palindrome_after_42_iterations())
+        .collect::<Vec<u128>>()
+        .par_iter()
+        .filter(|n| n.becomes_palindrome_after_n_iterations(42))
         .sum();
     println!("{}", sum);
 }
@@ -8,7 +12,7 @@ fn main() {
 trait PalindromeExtensions {
     fn reverse(self) -> u128;
     fn is_palindrome(self) -> bool;
-    fn becomes_palindrome_after_42_iterations(self) -> bool;
+    fn becomes_palindrome_after_n_iterations(self, iterations: u8) -> bool;
 }
 
 impl PalindromeExtensions for u128 {
@@ -30,18 +34,15 @@ impl PalindromeExtensions for u128 {
     fn is_palindrome(self) -> bool {
         self == self.reverse()
     }
-    fn becomes_palindrome_after_42_iterations(self) -> bool {
+    fn becomes_palindrome_after_n_iterations(self, iterations: u8) -> bool {
         let mut n = self;
-        for i in 1..43 {
+        for i in 1..(iterations + 1) {
             n = n.checked_add(n.reverse()).expect(&format!(
                 "overflow checking {} on iteration {} : {}",
                 self, i, n
             ));
             if n.is_palindrome() {
-                return match i {
-                    42 => true,
-                    _ => false,
-                };
+                return i == iterations;
             }
         }
         false
@@ -66,5 +67,17 @@ mod tests {
         assert!(55.is_palindrome());
         assert!((110 + 110.reverse()).is_palindrome());
         assert_eq!(false, 123.is_palindrome());
+    }
+
+    #[test]
+    fn test_64_should_be_palindrome_after_2_iterations() {
+        assert!(64.becomes_palindrome_after_n_iterations(2));
+    }
+
+    #[test]
+    fn test_561_should_be_palindrome_after_3_iterations() {
+        assert_eq!(false, 561.becomes_palindrome_after_n_iterations(2));
+        assert!(561.becomes_palindrome_after_n_iterations(3));
+        assert_eq!(false, 561.becomes_palindrome_after_n_iterations(4));
     }
 }
