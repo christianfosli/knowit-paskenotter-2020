244245d3528c76d733546cdd6dbefc9abc83336c
Christian Fosli <christian.fosli@capgemini.com>
2020-11-21 22:36:40 +0100

    Simplify fetch with FSharp.Data.Http and FSharp 5 `#r nuget`

--- luke1-fs/luke1.fsx
+++ luke1-fs/luke1.fsx
@@ -1,23 +1,14 @@
-open System.Net.Http
-
+// Luke 1 -- Knowit Påskekalender
 // This file contains numbers from 1 to 100_000, except for one number. Which number is missing?
 // https://gist.githubusercontent.com/knowitkodekalender/f962392f90e181377dd51de6abe3b480/raw/c0ef3c2762d3f7bf9016b06d2d6b1fdce58f36cd/numbers.txt
 
-let fetchNumbers () =
-    async {
-        let client = new HttpClient()
+#r "nuget: FSharp.Data"
 
-        let! res =
-            client.GetAsync
-                ("https://gist.githubusercontent.com/knowitkodekalender/f962392f90e181377dd51de6abe3b480/raw/c0ef3c2762d3f7bf9016b06d2d6b1fdce58f36cd/numbers.txt")
-            |> Async.AwaitTask
+open FSharp.Data
 
-        return! res.Content.ReadAsStringAsync() |> Async.AwaitTask
-    }
-
-fetchNumbers ()
-|> Async.RunSynchronously
-|> fun content -> content.Split(",")
+Http.RequestString
+    "https://gist.githubusercontent.com/knowitkodekalender/f962392f90e181377dd51de6abe3b480/raw/c0ef3c2762d3f7bf9016b06d2d6b1fdce58f36cd/numbers.txt"
+|> fun numberString -> numberString.Split(",")
 |> Seq.map int
 |> Set.ofSeq
 |> Set.difference (Set.ofSeq [ 1 .. 100_000 ])
