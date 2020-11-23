38ddcdd825984c9819c9b6ad70da2624898f6681
Christian Fosli <christian.fosli@capgemini.com>
2020-11-23 22:00:59 +0100

    Remove unneeded paranthesis

--- luke1-fs/luke1.fsx
+++ luke1-fs/luke1.fsx
@@ -8,7 +8,7 @@ open FSharp.Data
 
 Http.RequestString
     "https://gist.githubusercontent.com/knowitkodekalender/f962392f90e181377dd51de6abe3b480/raw/c0ef3c2762d3f7bf9016b06d2d6b1fdce58f36cd/numbers.txt"
-|> fun numberString -> numberString.Split(",")
+|> fun numberString -> numberString.Split ","
 |> Seq.map int
 |> Set.ofSeq
 |> Set.difference (Set.ofSeq [ 1 .. 100_000 ])
